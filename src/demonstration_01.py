86b418e948221a71ceab4fb8be59fafb670192fd
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-20 14:15:31 -0400

    the last n elements from a list

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -13,4 +13,14 @@ Notes:
 
 
 def last(a, n):
-    # Your code here
+  if n > len(a):
+    return 'invalid'
+  if n == 0:
+    return []
+  return a[-n:]
+
+
+print(last([1, 2, 3, 4, 5], 1))
+print(last([4, 3, 9, 9, 7, 6], 3))
+print(last([1, 2, 3, 4, 5], 7))
+print(last([1, 2, 3, 4, 5], 0)) 
